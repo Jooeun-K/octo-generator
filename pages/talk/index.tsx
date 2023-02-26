07678750232acd47547fbffa49b79d198f6434c5
Jooeun-K <gangjoo93@gmail.com>
2023-02-26 17:14:41 +0900

    chore: preview 폴더명 변경

--- pages/talk/index.tsx
+++ pages/talk/index.tsx
@@ -1,5 +1,5 @@
 import TalkEditor from "@/components/talk/Editor";
-import TalkPreview from "@/components/talk/Preview";
+import TalkPreview from "@/components/talk/PreviewCom";
 
 const Talk = () => {
   return (
