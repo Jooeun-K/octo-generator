8f5a2629b0eba1013fcaca02e228c4bb9fc06ae1
Jooeun-K <gangjoo93@gmail.com>
2023-02-26 16:58:10 +0900

    feat: talk preview 컴포넌트 생성

---
+++ components/talk/preview/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./TalkPreview.module.css";
+
+const TalkPreview = () => {
+  return <div className={styles.wrapper}>preview</div>;
+};
+
+export default TalkPreview;
---
+++ pages/talk/index.tsx
@@ -0,0 +1,12 @@
+import TalkPreview from "@/components/talk/preview";
+
+const Talk = () => {
+  return (
+    <>
+      <TalkPreview />
+      <div>입력부</div>
+    </>
+  );
+};
+
+export default Talk;
