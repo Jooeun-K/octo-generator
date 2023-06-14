bd35dab0f4b75c2511c7da67377e552f48e22447
Jooeun-K <gangjoo93@gmail.com>
2023-06-14 23:03:09 +0900

    build: storybook build 설정 변경 및 chromatic 로그 파일 gitignore 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  stories: ["../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
