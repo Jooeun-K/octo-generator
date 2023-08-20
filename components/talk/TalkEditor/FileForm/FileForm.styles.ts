0cae1c05f07f46e73c1eef7a632dfc5c997d16f9
Jooeun-K <gangjoo93@gmail.com>
2023-08-21 02:08:43 +0900

    chore: 미사용 FileForm 컴포넌트 삭제 및 미사용 의존성 제거

--- components/talk/TalkEditor/FileForm/FileForm.styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from '@/styles/stitches.config'
-
-export const ImageBox = styled('div', {
-  display: 'flex',
-  border: '1px solid $GRAY400',
-  background: 'white',
-  padding: '10px',
-  margin: '10px 0',
-})
